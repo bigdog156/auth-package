3a0e0a0ae345b49e7841d0389db39b480b3e52aa
lamlt <lamlt@galaxy.one>
2024-06-09 21:10:41 +0700

    Setup

--- Package.swift
+++ Package.swift
@@ -5,19 +5,34 @@ import PackageDescription
 
 let package = Package(
     name: "auth-package",
+    platforms: [
+        .iOS(.v13)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "auth-package",
-            targets: ["auth-package"]),
+            targets: ["auth-package"]
+        ),
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/supabase-community/supabase-swift.git",
+            from: "2.0.0"
+        )
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "auth-package"),
+            name: "auth-package",
+            dependencies: [
+                .product(name: "Supabase", package: "supabase-swift"),
+            ]
+        ),
         .testTarget(
             name: "auth-packageTests",
-            dependencies: ["auth-package"]),
+            dependencies: ["auth-package"]
+        ),
     ]
 )
---
+++ README.md
@@ -0,0 +1,8 @@
+//
+//  README
+//  auth-package
+//
+//  Created by Le Thach lam on 6/6/24.
+//
+
+Name: Package Authentication using Supabase
